442d710d78d3537614c73cb82d08e3a7026b5339
Naman Kumar <namankumar1303@gmail.com>
2024-07-10 03:43:39 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Hate-speech-classification",
+    version="0.0.1",
+    author="NAMAN KUMAR",
+    author_email="namankumar1303@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
